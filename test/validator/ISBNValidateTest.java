826d38b8db798a44406f6648a3f1a50c50145946
shezac13 <shezac13>
2025-05-30 09:24:42 -0400

    combine tests for dif cadre

--- test/validator/ISBNValidateTest.java
+++ test/validator/ISBNValidateTest.java
@@ -45,65 +45,42 @@ public class ISBNValidateTest {
 	public void tearDown() throws Exception {
 	}
 
+		/* Cadre 1, 3, 6, 8 et 9*/
 	@Test
 	public void test1() {
-		String ISBN = "1/111-11-1111";
+		String ISBN = "Aao1/111-1yyb1-11@11Uoou";
 		String expected = "1-111-11111-1";
 		Assert.assertEquals(expected, ISBNValidate.tidyISBN10or13InsertingDashes(ISBN));
 	}
 	
-	// maybe just put Null instead for illegal format
+	/* Cadre 2 */
 	@Test(expected = IllegalArgumentException.class)
 	public void test2() {
-		String ISBN = "11@1111111";
-		ISBNValidate.tidyISBN10or13InsertingDashes(ISBN);
+		ISBNValidate.tidyISBN10or13InsertingDashes(null);
 	}
-	/*Cas 3 a 5*/
+	/* Cadre 4, 6, 8, 9, 10 */
 	@Test()
 	public void test3() {
-		String ISBN = "0596520689";
-		String expected = "0-596-52068-9";
-		Assert.assertEquals(expected, ISBNValidate.tidyISBN10or13InsertingDashes(ISBN));
-	}
-	@Test()
-	public void test4() {
-		String ISBN = "9780596520687";
+		String ISBN = "a-978-059@6520JK687";
 		String expected = "978-0-596-52068-7";
 		Assert.assertEquals(expected, ISBNValidate.tidyISBN10or13InsertingDashes(ISBN));
 	}
-	// 13 digit ISBN must start with 978 or 979
+	/* Cadre 11 */
 	@Test(expected = IllegalArgumentException.class)
-	public void test41() {
+	public void test4() {
 		String ISBN = "1111111111111";
 		ISBNValidate.tidyISBN10or13InsertingDashes(ISBN);
 	}
+	/*Cadre 5*/
 	@Test(expected = IllegalArgumentException.class)
 	public void test5() {
 		String ISBN = "11";
 		ISBNValidate.tidyISBN10or13InsertingDashes(ISBN);
 	}
-	
-	@Test()
-	public void test6() {
-		String ISBN = "";
-	}
+	/* Cadre 7*/
 	@Test(expected = IllegalArgumentException.class)
-	public void test7() {
+	public void test6() {
 		String ISBN = "9781111111111";
 		ISBNValidate.tidyISBN10or13InsertingDashes(ISBN);
-	}
-	
-	@Test()
-	public void test8() {
-		String ISBN = "abcd1111111111efg";
-		String expected = "1-111-11111-1";
-		Assert.assertEquals(expected, ISBNValidate.tidyISBN10or13InsertingDashes(ISBN));
-	}
-	
-	@Test()
-	public void test9() {
-		String ISBN = "1@1111$111&11";
-		String expected = "1-111-11111-1";
-		Assert.assertEquals(expected, ISBNValidate.tidyISBN10or13InsertingDashes(ISBN));
 	}
 }
